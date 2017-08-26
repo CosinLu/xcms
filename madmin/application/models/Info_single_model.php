0f0eb99a8fde4811a68f53b973a0a2e24d2d7cba
MengXianghan <1056811341@qq.com>
2017-08-26 20:55:09 +0800

    优化代码

--- madmin/application/models/Info_single_model.php
+++ madmin/application/models/Info_single_model.php
@@ -18,7 +18,7 @@ class Info_single_model extends M_Model
         $this->db->select('t.name,t.id');
         $this->db->select('t1.summary,t1.content');
         $this->db->from('info_col as t');
-        $this->db->where('t.id', $this->cid);
+        $this->db->where('t.id', $cid);
         $this->db->join('info_single as t1', 't1.cid=t.id', 'left');
         $res = $this->db->get()->row_array();
         return $res;
